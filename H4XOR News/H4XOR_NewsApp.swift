7bfa007a60087a317475481929f8f72a5c940727
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-11-26 16:24:45 +0300

    initial commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by malakoipechyva on 26.11.20.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

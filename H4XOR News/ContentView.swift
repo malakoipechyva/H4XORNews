7d7912e750fc36fead542d7465dd42167e61ff5b
Yauhen  Chyrko <chirko.eugene@gmail.com>
2020-11-26 18:28:07 +0300

    add observer pattern

--- H4XOR News/ContentView.swift
+++ H4XOR News/ContentView.swift
@@ -8,12 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+  
+  @ObservedObject var networkManager = NetworkManager()
+  
   var body: some View {
     NavigationView {
-      List(posts) { post in
-        Text(post.title)
+      List(networkManager.posts) { post in
+        HStack {
+          Text(String(post.points))
+          Text(post.title)
+        }
       }
       .navigationBarTitle("H4X0R News")
+    }
+    .onAppear {
+      networkManager.fetchData()
     }
   }
 }
@@ -24,8 +33,8 @@ struct ContentView_Previews: PreviewProvider {
     }
 }
 
-let posts = [
-  Post(id: "1", title: "hello"),
-  Post(id: "2", title: "Vitau"),
-  Post(id: "3", title: "Hi")
-]
+//let posts = [
+//  Post(id: "1", title: "hello"),
+//  Post(id: "2", title: "Vitau"),
+//  Post(id: "3", title: "Hi")
+//]
